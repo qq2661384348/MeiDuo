87675ef234a14b1be360f9fe8220ab487dbcf2fd
Adouad <2661384348@qq.com>
2024-08-04 19:27:41 +0800

    jinja2在setting中的修复

--- MeiDuo/settings.py
+++ MeiDuo/settings.py
@@ -59,6 +59,7 @@ TEMPLATES = [
         ,
         'APP_DIRS': True,
         'OPTIONS': {
+            'environment': 'MeiDuo.utils.jinja2_env.environment',
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
@@ -67,6 +68,20 @@ TEMPLATES = [
             ],
         },
     },
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+
 ]
 
 WSGI_APPLICATION = 'MeiDuo.wsgi.application'
